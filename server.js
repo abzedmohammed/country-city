18f13572c4250098a22123a82a996555a2ca5344
abzedmohammed <ibraabzed@gmail.com>
2024-10-21 14:48:07 +0300

    fix running bug

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults({ static: "./build" });
-// const port = 3000;
-// server.use(middlewares);
-// server.use(router);
-// server.listen(port);
-
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
-
+const port = 3000;
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
 server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
+server.listen(port);
 
-// Export the Server API
-module.exports = server;
+
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const fs = require('fs');
+// const path = require('path');
+// const middlewares = jsonServer.defaults();
+
+// const data = JSON.parse(
+// 	fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8')
+// );
+// const router = jsonServer.router(data);
+
+// server.use(middlewares);
+
+// server.use(
+// 	jsonServer.rewriter({
+// 		'/api/*': '/$1',
+// 	})
+// );
+// server.use(router);
+// server.listen(3000, () => {
+// 	console.log('JSON Server is running');
+// });
+
+// module.exports = server;
